a837f77c812e4b3d4c45150af54c842327c1b8dd
aishu-ch <aishu.chinnaiya1112@gmail.com>
2024-06-14 15:29:12 +0800

    feat: css for login, register, home and navbar

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,4 +1,4 @@
-import {useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router";
 import { registerNewUser } from "../utils/axios/userAPIs";
 
@@ -19,14 +19,17 @@ const Register = () => {
   };
 
   return (
-    <div>
-      <div>
-        <h1>SnapShare</h1>
-        <h2>Sign up to see photos and videos from your friends.</h2>
+    <div className="mx-auto w-[396px] h-screen">
+      <div className="border my-8 pb-6 bg-white border-gray-300">
+        <h1 className="text-center text-3xl my-4 py-6">SnapShare</h1>
+        <h2 className="my-4 pb-8 mx-8 text-center font-bold text-gray-400 text-lg">
+          Sign up to see photos and videos from your friends.
+        </h2>
         <div>
-          <form>
+          <form className="mx-8 py-1 flex flex-col gap-4">
             <input
               type="text"
+              className="bg-gray-50 mx-8 py-1 placeholder:text-xs focus:outline-1 focus:outline-gray-400 placeholder:text-gray-400 border rounded-sm p-2"
               placeholder="Email Address"
               name="email"
               onChange={changeHandler}
@@ -34,6 +37,7 @@ const Register = () => {
             />
             <input
               type="text"
+              className="bg-gray-50 mx-8 py-1 placeholder:text-xs focus:outline-1 focus:outline-gray-400 placeholder:text-gray-400 border rounded-sm p-2"
               placeholder="Full Name"
               name="fullName"
               onChange={changeHandler}
@@ -41,6 +45,7 @@ const Register = () => {
             />
             <input
               type="text"
+              className="bg-gray-50 mx-8 py-1 placeholder:text-xs focus:outline-1 focus:outline-gray-400 placeholder:text-gray-400 border rounded-sm p-2"
               placeholder="Username"
               name="userName"
               onChange={changeHandler}
@@ -48,6 +53,7 @@ const Register = () => {
             />
             <input
               type="password"
+              className="bg-gray-50 mx-8 py-1 placeholder:text-xs focus:outline-1 focus:outline-gray-400 placeholder:text-gray-400 border rounded-sm p-2"
               placeholder="Password"
               name="password"
               onChange={changeHandler}
@@ -55,6 +61,7 @@ const Register = () => {
             />
             <button
               type="button"
+              className="bg-sky-500 text-center mx-8 text-sm py-1 rounded-sm text-white font-bold"
               onClick={() =>
                 registerNewUser(
                   registrationData.email,
@@ -69,10 +76,15 @@ const Register = () => {
           </form>
         </div>
       </div>
-      <div>
-        <h1>
+      <div className="border bg-white text-center py-4 border-gray-300">
+        <h1 className="text-sm">
           Have an account?{" "}
-          <span onClick={() => navigate("/login")}>Log in</span>
+          <span
+            onClick={() => navigate("/login")}
+            className="text-sky-500 font-bold cursor-pointer"
+          >
+            Log in
+          </span>
         </h1>
       </div>
     </div>
