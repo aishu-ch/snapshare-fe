a9bd29a8e81c4ce328c4fe79e9f5a75556e1f958
aishu-ch <aishu.chinnaiya1112@gmail.com>
2024-06-14 21:45:12 +0800

    feat: profile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,8 +5,10 @@ import { HomeSelected, HomeUnselected } from "./icons/Home";
 import { NotificationsUnselected } from "./icons/Notifications";
 import { SearchBtnIcon } from "./icons/Search";
 import { Link } from "react-router-dom";
-import MoreModal from "./MoreModal";
+import { getProfileUsernameandProfilePic } from "../utils/axios/profileAPIs";
+import Search from "./Search";
 import { MenuIcon } from "./icons/Menu";
+import MoreModal from "./MoreModal";
 
 const Navbar = () => {
   const [profile, setProfile] = useState({
@@ -14,13 +16,19 @@ const Navbar = () => {
     profilePic: "",
   });
 
+  useEffect(() => {
+    getProfileUsernameandProfilePic()
+      .then((res) => setProfile(res))
+      .catch((err) => console.log(err));
+  }, []);
+
   const [openMoreMenu, setOpenMoreMenu] = useState(false);
 
   return (
     <>
       <nav className="h-screen relative z-50 bg-white border-r border-gray-300 sticky top-0 w-[72px] md:w-fit">
         <h1 className="text-4xl px-12 mt-4 invisible md:visible">SnapShare</h1>
-        <div>
+        <div className="text-xl px-4 mx-auto h-full pt-8">
           <Link
             to={"/"}
             className="pl-2 flex gap-4 py-2 my-2 hover:bg-gray-50 hover:rounded-full"
@@ -28,7 +36,9 @@ const Navbar = () => {
             <HomeSelected />
             <h1 className="invisible md:visible">Home</h1>
           </Link>
-          <ul className="pl-2 flex gap-4 py-2 my-2 hover:bg-gray-50 hover:rounded-full hover:cursor-pointer">
+          <ul
+            className="pl-2 flex gap-4 py-2 my-2 hover:bg-gray-50 hover:rounded-full hover:cursor-pointer"
+          >
             <SearchBtnIcon />
             <h1 className="invisible md:visible">Search</h1>
           </ul>
@@ -41,12 +51,26 @@ const Navbar = () => {
           </Link>
           <ul className="pl-2 flex gap-4 py-2 my-2 hover:bg-gray-50 hover:rounded-full">
             <NotificationsUnselected />
-            <h1 className="invisible md:visible">Notifications</h1>
+            <h1 className="invisible md:visible" onClick={() => console.log()}>
+              Notifications
+            </h1>
           </ul>
-          <ul className="pl-2 flex gap-4 py-2 my-2 cursor-pointer hover:bg-gray-50 hover:rounded-full">
+          <ul
+            className="pl-2 flex gap-4 py-2 my-2 cursor-pointer hover:bg-gray-50 hover:rounded-full"
+          >
             <CreateUnselected />
             <h1 className="invisible md:visible">Create</h1>
           </ul>
+          <Link
+            to={`/profile/${profile.userName}`}
+            className="flex gap-4 py-2 my-2 pl-2 hover:bg-gray-50 hover:rounded-full"
+          >
+            <img
+              className="w-[32px] h-[32px] rounded-full"
+              src={profile.profilePic}
+            />
+            <h1 className="invisible md:visible">{profile.userName}</h1>
+          </Link>
           <ul
             className="pl-2 flex gap-4 py-2 mb-auto cursor-pointer hover:bg-gray-50 hover:rounded-full relative"
             onClick={() => setOpenMoreMenu(!openMoreMenu)}
@@ -55,8 +79,12 @@ const Navbar = () => {
             <MenuIcon />
             <h1 className="invisible md:visible">More</h1>
           </ul>
-        </div>
+          <ul></ul>
+                 </div>
       </nav>
+      <div className="relative">
+        <Search />
+      </div>
     </>
   );
 };
---
+++ src/utils/axios/profileAPIs.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const baseURL = process.env.REACT_APP_BACKEND_BASE_URL;
+const profileURL = process.env.REACT_APP_BACKEND_PROFILE_URL;
+
+// @ts-ignore
+const token = JSON.parse(JSON.stringify(localStorage.getItem("token")));
+
+axios.defaults.headers.common = { Authorization: `Bearer ${token}` };
+
+export const getProfileUsernameandProfilePic = async () => {
+  try {
+    const data = await axios.get(`${baseURL}${profileURL}/getprofiledata`);
+    return data.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getProfileId = async () => {
+  try {
+    const data = await axios.get(`${baseURL}${profileURL}/getprofiledata`);
+    return data.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
